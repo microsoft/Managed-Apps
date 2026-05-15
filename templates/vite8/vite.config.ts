c5db8f32c8b8453ae1ba9e9483bf3ded7e248e0a
Chamu-Rajasekera <chrajase@microsoft.com>
2026-05-15 15:44:34 -0700

    Managed App changes

--- templates/vite8/vite.config.ts
+++ templates/vite8/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { managedApps } from "@microsoft/managed-apps-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), managedApps()],
+});
